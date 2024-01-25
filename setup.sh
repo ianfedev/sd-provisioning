0312c2ec8ca5140df0729ce4cb54047e4bfebd0e
Ian Castaño <ianfe@martina.com.co>
2024-01-24 20:06:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,9 +19,6 @@ PIP_PACKAGES=(
 
 EXTENSIONS=(
     "https://github.com/Mikubill/sd-webui-controlnet"
-    "https://github.com/d8ahazard/sd_dreambooth_extension"
-    "https://github.com/deforum-art/sd-webui-deforum"
-    "https://github.com/adieyal/sd-dynamic-prompts"
     "https://github.com/ototadana/sd-face-editor"
     "https://github.com/Bing-su/adetailer"
     "https://github.com/hako-mikan/sd-webui-regional-prompter"
