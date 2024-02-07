7cd9f23834813f20f20e0b1deac44ec31ac110bf
Ian Castaño <ianfe@martina.com.co>
2024-02-06 21:22:36 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,9 +30,7 @@ CHECKPOINT_MODELS=(
 )
 
 LORA_MODELS=(
-    "https://huggingface.co/MomlessTomato/sdxl-maki-nishikino/resolve/main/id_maki_nishikino-000010.safetensors"
-    "https://huggingface.co/MomlessTomato/sdxl-maki-nishikino/resolve/main/id_maki_nishikino-000009.safetensors"
-    "https://huggingface.co/MomlessTomato/sdxl-maki-nishikino/resolve/main/id_maki_nishikino-000008.safetensors"
+    "https://huggingface.co/MomlessTomato/sdxl-maki-nishikino/resolve/main/id_maki_nishikino.safetensors"
     "https://huggingface.co/MomlessTomato/sdxl-maki-nishikino/resolve/main/id_maki_nishikino_sdxl.safetensors"
     "https://huggingface.co/MomlessTomato/sdxl-shioriko-mifune/resolve/main/id_shioriko_mifune_sdxl.safetensors"
 )
