a956e0500ee5d2d48be909a56bdf7c8ea6559fd0
Ian Castaño <ianfe@martina.com.co>
2024-08-25 17:39:39 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,9 +30,7 @@ CHECKPOINT_MODELS=(
 )
 
 LORA_MODELS=(
-    "https://huggingface.co/MomlessTomato/aqours/resolve/main/id_ai_miyashita.safetensors"
-    "https://huggingface.co/MomlessTomato/aqours/resolve/main/id_setsuna_yuki.safetensors"
-    "https://huggingface.co/MomlessTomato/aqours/resolve/main/id_chika_takami.safetensors"
+    "https://huggingface.co/MomlessTomato/nijigasaki/resolve/main/id_ayumu_uehara.safetensors"
     "https://huggingface.co/MomlessTomato/aqours/resolve/main/id_ruby_kurosawa.safetensors"
     "https://huggingface.co/MomlessTomato/aqours/resolve/main/great_lighting.safetensors"
     "https://huggingface.co/MomlessTomato/aqours/resolve/main/openxl_handsfix.safetensors"
