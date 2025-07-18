3bd3c0193a3e67ef8db264d60572d1fb3be2fbf7
Ian C. <ianfe@martina.com.co>
2025-07-18 00:12:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ EXTENSIONS=(
     "https://github.com/Mikubill/sd-webui-controlnet"
     "https://github.com/toshiaki1729/stable-diffusion-webui-dataset-tag-editor"
     "https://github.com/Bing-su/adetailer"
-    "https://github.com/ilian6806/stable-diffusion-webui-state"
+    "https://github.com/ljleb/sd-webui-freeu"
 )
 
 AD_DETAILER_MODEL=(
